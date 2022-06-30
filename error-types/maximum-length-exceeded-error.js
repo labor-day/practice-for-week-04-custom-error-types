1772b675e8d3100b9413621d1362d95479ac3b01
labor-day <shaffer.scott@yahoo.com>
2022-06-30 15:46:57 -0600

    implemented custom errors

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -2,6 +2,28 @@ const ValidationError = require('./validation-error');
 
 // Your code here
 
+class MaximumLengthExceededError extends ValidationError {
+
+  constructor(difference, message = "Maximum length exceeded", ...params) {
+
+    super(...params);
+
+    // if (Error.captureStackTrace) {
+    //   Error.captureStackTrace(this, MaximumLengthExceededError);
+    // }
+
+    this.name = "MaximumLengthExceededError";
+    this.message = message;
+
+    if (difference) {
+      this.message += ` by ${difference}`;
+    }
+
+  }
+
+
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,5 +1,20 @@
 // Your code here
 
+class ValidationError extends Error {
+
+  constructor(message = "Invalid input", ...params) {
+    super(...params);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+
+    this.message = message;
+    this.name = "ValidationError";
+
+  }
+}
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
